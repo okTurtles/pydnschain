9446a74dbd266e9a5bb906d2cdbd21b9e695a0e8
charapod <charapod@gmail.com>
2014-11-14 20:52:51 +0200

    Added a test for a complete request/response

--- dnschain/test/test_server.py
+++ dnschain/test/test_server.py
@@ -4,13 +4,14 @@ import unittest
 import dnschain
 from dnschain.server import Server
 
+import json
 
 class TestServerInstantiation(unittest.TestCase):
 
-    @mock.patch('dnschain.server.urllib.FancyURLopener')
+    @mock.patch('dnschain.server.urllib2.urlopen')
     def setUp(self, mock_opener):
         self.mock_opener = mock_opener
-        self.assertIs(dnschain.server.urllib.FancyURLopener, self.mock_opener)
+        self.assertIs(dnschain.server.urllib2.urlopen, self.mock_opener)
         self.dnschain_server = Server("0.0.0.0", "FAKEFINGERPRINT")
 
     def test_invalid_address(self):
@@ -29,10 +30,10 @@ class TestServerInstantiation(unittest.TestCase):
 class TestServerLookup(unittest.TestCase):
 
     @mock.patch('dnschain.server.json.loads')
-    @mock.patch('dnschain.server.urllib.FancyURLopener')
+    @mock.patch('dnschain.server.urllib2.urlopen')
     def setUp(self, mock_opener, mock_json_loads):
         self.mock_opener = mock_opener
-        self.assertIs(dnschain.server.urllib.FancyURLopener, self.mock_opener)
+        self.assertIs(dnschain.server.urllib2.urlopen, self.mock_opener)
         self.dnschain_server = Server("0.0.0.0", "FAKEFINGERPRINT")
         self.dnschain_server.lookup("greg")
 
@@ -44,6 +45,35 @@ class TestServerLookup(unittest.TestCase):
 
     def test_no_match(self):
         pass
+
+
+class TestDNSChainLookup(unittest.TestCase):
+
+    def setUp(self):
+        self.dnschain_server = Server("192.184.93.146", "NOTYETIMPLEMENTED")
+        self.test_cases = {"id/dionyziz": "dionyziz@gmail.com",
+                           "id/greg": ["contact@taoeffect.com", "hi@okturtles.com"]}
+        self.responses = {}
+        for name in self.test_cases:
+            self.responses[name] = self.dnschain_server.lookup(name)
+        
+    def test_valid_JSON(self):
+        for response in self.responses.itervalues():
+            try:
+                json.dumps(response)
+            except:
+                self.fail("Response was not in valid JSON format")
+        
+    def test_contains_email(self):
+        for response in self.responses.itervalues():
+            self.assertIn("email", response)
+
+    def test_correct_email(self):
+        for name in self.test_cases:
+            if "email" not in self.responses[name]:
+                self.skipTest('Response does not contain key "email"')
+            self.assertEqual(self.test_cases[name], 
+                             self.responses[name]["email"])
 
 
 # class MaybeSubClassingIsTheCleanestWayForward
