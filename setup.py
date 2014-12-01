9032e4ef7e2dfc42712e322e36f3194decaf6d6f
Greg Slepak <contact@taoeffect.com>
2014-11-30 20:40:15 -0800

    Updated metadata

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
     name="dnschain",
     version=dnschain.__version__,
     url='https://github.com/okturtles/pydnschain',
-    license='MPL',
-    description="A Python DNSChain library",
-    author='Greg Slepak',
+    license='MPL 2.0',
+    description="Python library to perform DNSChain lookups",
+    author='okTurtles Foundation',
     author_email='hi@okturtles.com',
     packages=find_packages(),
     classifiers=[
@@ -20,5 +20,6 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Topic :: Security :: Cryptography',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
     ],
 )
